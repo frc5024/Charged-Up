5d0e56bb3fc3cd1b4b6bc06f7fde24b644b74769
Connor <quinconn312@gotvdsb.ca>
2023-01-21 13:23:13 -0500

    AutoLevel

---
+++ src/main/java/frc/robot/commands/AutoLevel.java
@@ -0,0 +1,16 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Commands;
+
+public final class AutoLevel {
+  /** Example static factory for an autonomous command. */
+  public static CommandBase exampleAuto(ExampleSubsystem subsystem) {
+    return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
+  }
+
+  private AutoLevel() {
+    throw new UnsupportedOperationException("This is a utility class!");
+  }
+}
