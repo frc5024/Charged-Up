2d9854c72f6886a364d463459307b4afcbc78150
Kaleb Dos Santos <kalebdossantos72504@gmail.com>
2023-01-21 14:06:55 -0500

    OI Basic formatting

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -2,8 +2,20 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
 
+
 public class OI {
 
-    
+    private static OI instance = null;
+
+    public static OI getInstance(){
+        if (instance == null) {
+            instance = new OI();
+        }
+        return instance;
+    }
+
+    private OI() {
+        
+    }
     
 }
