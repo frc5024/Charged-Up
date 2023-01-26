3fdf1183eab3b1045feb861424a3372c4dffd6ea
Vinicius <viniciuscoelho.cv@gmail.com>
2023-01-26 17:43:06 -0500

    Added state machines from lib5k

---
+++ src/main/java/frc/robot/liblite/RobotLogger.java
@@ -0,0 +1,280 @@
+package frc.robot.liblite;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import edu.wpi.first.wpilibj.Notifier;
+import edu.wpi.first.wpilibj.RobotBase;
+
+
+/**
+ * A threaded logger for use by all robot functions
+ */
+
+public class RobotLogger {
+    private static RobotLogger instance = null;
+    private Notifier notifier;
+    ArrayList<String> periodic_buffer = new ArrayList<String>();
+  
+    private double bootTime;
+
+    // Simulation logfile
+    private FileWriter simWriter;
+
+    /**
+     * Log level
+     * 
+     * The kRobot level will immediately push to the console, everything else is
+     * queued until the next notifier cycle
+     */
+    public enum Level {
+        kRobot("INFO"), kInfo("INFO"), kWarning("WARNING"), kDebug("DEBUG"), kLibrary("INFO");
+
+        public String name;
+
+        /**
+         * Create a log level
+         * 
+         * @param name String name
+         */
+        private Level(String name) {
+            this.name = name;
+        }
+    }
+
+    /**
+     * Create the RobotLogger instance
+     */
+    private RobotLogger() {
+        this.notifier = new Notifier(this::pushLogs);
+
+        // set boot time
+        this.bootTime = (double) System.currentTimeMillis() / 1000.0;
+
+        // Try to load sim logger
+        if (RobotBase.isSimulation()) {
+            try {
+                simWriter = new FileWriter("./FRC_UserProgram.log");
+                simWriter.write("");
+            } catch (IOException e) {
+                System.out.println("Not writing to simulation logfile because of error");
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    /**
+     * Enable logging to a USB
+     * 
+     * @param logger USB logger object
+     */
+   
+    /**
+     * Start the periodic logger
+     * 
+     * @param period The logging notifier period time in seconds
+     */
+    public void start(double period) {
+        this.notifier.startPeriodic(period);
+    }
+
+    /**
+     * Get a RobotLogger instance
+     * 
+     * @return The current RobotLogger
+     */
+    public static RobotLogger getInstance() {
+        if (instance == null) {
+            instance = new RobotLogger();
+        }
+        return instance;
+    }
+
+    /**
+     * Write a log message to the logfile and message buffer.
+     * 
+     * @param msg  Log message (String.format style)
+     * @param args Format arguments
+     */
+    public void log(String msg, Object... args) {
+        // Get stack trace
+        StackTraceElement lastMethod = Thread.currentThread().getStackTrace()[2];
+
+        // Log
+        log(lastMethod, Level.kInfo, msg, args);
+    }
+
+    /**
+     * Write a log message to the logfile and message buffer.
+     * 
+     * @param msg  Log message (String.format style)
+     * @param lvl  Log level
+     * @param args Format arguments
+     */
+    public void log(String msg, Level lvl, Object... args) {
+        // Get stack trace
+        StackTraceElement lastMethod = Thread.currentThread().getStackTrace()[2];
+
+        // Log
+        log(lastMethod, lvl, msg, args);
+    }
+
+    /**
+     * Write a log message to the logfile and message buffer. This is deprecated,
+     * and acts as a binding for old programs.
+     * 
+     * @param component Calling component name
+     * @param msg       Log message
+     */
+    @Deprecated(since = "July 2020", forRemoval = false)
+    public void log(String component, String msg) {
+
+        // Get stack trace
+        StackTraceElement lastMethod = Thread.currentThread().getStackTrace()[2];
+
+        // Log
+        log(lastMethod, Level.kInfo, msg);
+    }
+
+    /**
+     * Write a log message to the logfile and message buffer. This is deprecated,
+     * and acts as a binding for old programs.
+     * 
+     * @param component Calling component name
+     * @param msg       Log message
+     * @param log_level Log level
+     */
+    @Deprecated(since = "July 2020", forRemoval = false)
+    public void log(String component, String msg, Level log_level) {
+
+        // Get stack trace
+        StackTraceElement lastMethod = Thread.currentThread().getStackTrace()[2];
+
+        // Log
+        log(lastMethod, log_level, msg);
+    }
+
+    /**
+     * Write a log message to the logfile and message buffer
+     * 
+     * @param lastMethod Stack trace to calling method
+     * @param lvl        Log level
+     * @param messageF   String.format style string / message
+     * @param args       Any format arguments
+     */
+    private void log(StackTraceElement lastMethod, Level lvl, String messageF, Object... args) {
+
+        // Build message
+        String message = String.format(messageF, args);
+
+        // Get the caller's package name
+        String packageName = getPackageName(lastMethod);
+
+        // Get the current system time
+        double time = (double) System.currentTimeMillis() / 1000.0;
+
+        // Determine time-since-boot
+        double tsb = time - this.bootTime;
+
+        // Build log string
+        String log = String.format("%s at %.2fs: %s -> %s", lvl.name, tsb, packageName, message);
+
+        // If the log is robot level, push to console
+        if (lvl.equals(Level.kRobot)) {
+
+            // Write log NOW
+            System.out.println(log);
+
+           
+
+        } else {
+            // Add log to buffer
+            this.periodic_buffer.add(log);
+        }
+
+        // If simulation, write to sim file
+        if (RobotBase.isSimulation() && simWriter != null) {
+            try {
+                simWriter.append(log + "\n");
+                simWriter.flush();
+            } catch (IOException e) {
+                System.out.println("Failed to reflect sim log");
+            }
+        }
+
+    }
+
+    /**
+     * Gets a friendly package name for a StackTraceElement
+     * 
+     * @param element Element to get name for
+     * @return Friendly Name
+     */
+    protected static String getPackageName(StackTraceElement element) {
+
+        // Build the package and method string
+        String fullPackageName = element.getClassName();
+        String[] packageSegments = fullPackageName.split("[.]");
+        StringBuilder packageNameBuilder = new StringBuilder();
+
+        // Handle the package name being too short
+        if (packageSegments.length <= 3) {
+
+            // Just build the full package name
+            packageNameBuilder.append(fullPackageName);
+
+        } else {
+            // The idea here is to turn a package name that might look like:
+            // io.github.frc5024.y2020.darthraider.commands.autonomous.actions.cells.SetShooterOutput
+            // Into one that looks like:
+            // io...cells.SetShooterOutput
+
+            // Append the root package
+            packageNameBuilder.append(packageSegments[0]);
+
+            // Append the seperator
+            packageNameBuilder.append("...");
+
+            // Append the class's parent package
+            packageNameBuilder.append(packageSegments[packageSegments.length - 2]);
+            packageNameBuilder.append(".");
+
+            // Append the class name
+            packageNameBuilder.append(packageSegments[packageSegments.length - 1]);
+        }
+
+        // Get the method name
+        String methodName = element.getMethodName();
+
+        return String.format("%s::%s()", packageNameBuilder.toString(), methodName.toString());
+
+    }
+
+    /**
+     * Manually flush the logs (only use this in unit tests)
+     */
+    public void flush(){
+        pushLogs();
+    }
+
+    /**
+     * Push all queued messages to netconsole, the clear the buffer
+     */
+    private void pushLogs() {
+        try {
+            for (String x : this.periodic_buffer) {
+
+                System.out.println(x);
+
+            }
+            periodic_buffer.clear();
+        } catch (Exception e) {
+            System.out.println("Tried to push concurrently");
+        }
+
+    }
+
+}
---
+++ src/main/java/frc/robot/liblite/StateMachine.java
@@ -0,0 +1,163 @@
+package frc.robot.liblite;
+
+import java.util.HashMap;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+import javax.annotation.Nullable;
+
+import frc.robot.liblite.RobotLogger;
+import frc.robot.liblite.RobotLogger.Level;
+
+public class StateMachine<T> {
+    private RobotLogger logger = RobotLogger.getInstance();
+
+    // The state to be run in the event of an error with state handling. Should be
+    // an idle state
+    public T defaultStateKey;
+
+    // A mapping of all states to their key
+    public HashMap<T, StateHandler<T>> allStates = new HashMap<>();
+
+    // Tracker for the last state to be run
+    public T lastStateKey;
+    public T desiredStateKey;
+
+    // Telemetry data table
+    private String name;
+
+    /**
+     * Create a StateMachine
+     * 
+     * @param name Name of the machine (for logging)
+     */
+    public StateMachine(String name) {
+
+        // Configure telemetry
+        this.name = name;
+
+        // Set default console output
+        setConsoleHook(System.out::println);
+
+    }
+
+    /**
+     * Deprecated Method
+     * 
+     * @param hook hook
+     */
+    @Deprecated(since = "July 2020", forRemoval = true)
+    public void setConsoleHook(@Nullable Consumer<String> hook) {
+    }
+
+    /**
+     * Add a state to the StateMachine
+     * 
+     * @param key    State key
+     * @param action Action to be run
+     */
+    public void addState(T key, Consumer<StateMetadata<T>> action) {
+
+        // Construct a StateHandler
+        StateHandler<T> handler = new StateHandler<T>(key, this, action);
+
+        // Add to mapping
+        allStates.put(key, handler);
+        logger.log(String.format("Added state: %s", key.toString()));
+
+    }
+
+    /**
+     * Add a state to the StateMachine, and set it as the default. This state will
+     * be called upon any error in state handling, or when no state is set.
+     * 
+     * @param key    State key
+     * @param action Action to be run
+     */
+    public void setDefaultState(T key, Consumer<StateMetadata<T>> action) {
+
+        // Add the state to the map
+        addState(key, action);
+
+        // Set the default
+        defaultStateKey = key;
+        logger.log(String.format("Set state %s as default", key.toString()));
+
+        // Make this the current state
+        setState(key);
+
+    }
+
+    /**
+     * Remove a state from the StateMachine
+     * 
+     * @param key State key
+     */
+    public void removeState(T key) {
+
+        // Remove from allstates
+        allStates.remove(key);
+
+        // If default, remove too
+        if (defaultStateKey == key) {
+            defaultStateKey = null;
+        }
+
+        logger.log(String.format("Removed state: %s", key.toString()));
+
+    }
+
+    /**
+     * Update the machine. This MUST be called periodically
+     */
+    public void update() {
+
+        // If the desired state key is null, and the default is null, we can't do
+        // anything
+        if (desiredStateKey == null && defaultStateKey == null) {
+            return;
+        }
+
+        // If the desired state key is null, overwrite it with the default key.
+        boolean defaultStateWasOverridden = false;
+        if (desiredStateKey == null) {
+            desiredStateKey = defaultStateKey;
+            defaultStateWasOverridden = true;
+        }
+
+        // If the current, and last state keys differ, this is the first run of the
+        // state
+        boolean isNew = lastStateKey == null || desiredStateKey == null || defaultStateWasOverridden
+                || !lastStateKey.equals(desiredStateKey);
+
+        // Set the last state key
+        // This must be placed above state.call()
+        lastStateKey = desiredStateKey;
+
+        // Handle the state
+        StateHandler<T> state = allStates.get(desiredStateKey);
+        state.call(isNew, lastStateKey);
+    }
+
+    /**
+     * Set the StateMachine's state
+     * 
+     * @param key State key
+     */
+    public void setState(T key) {
+        if (desiredStateKey != null && !desiredStateKey.equals(key)) {
+            logger.log("Switching to state: %s", Level.kDebug, key);
+        }
+        desiredStateKey = key;
+    }
+
+    /**
+     * Get the system's current state
+     * 
+     * @return current state
+     */
+    public @Nullable T getCurrentState() {
+        return desiredStateKey;
+    }
+
+}
