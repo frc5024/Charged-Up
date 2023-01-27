dd2fafc5be0e8c23701b77d3e29375fd6bbcde2a
Vinicius <viniciuscoelho.cv@gmail.com>
2023-01-27 17:29:22 -0500

    Imports for pneumatics and basic code, needs OI

--- src/main/java/frc/robot/subsystems/Drawer.java
+++ src/main/java/frc/robot/subsystems/Drawer.java
@@ -1,11 +1,16 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.liblite.StateMachine;
 import frc.robot.liblite.StateMetadata;
 
 public class Drawer extends SubsystemBase {
     private static Drawer mInstance = null;
+
+    private DoubleSolenoid extender;
 
     //Makes it a singleton
     public static Drawer getInstance() {
@@ -35,31 +40,38 @@ public class Drawer extends SubsystemBase {
         stateMachine.addState(drawerStates.EMPTYOUT, this::handleEmptyOut);
         stateMachine.addState(drawerStates.STOREDIN, this::handleStoredIN);
         stateMachine.addState(drawerStates.STOREDOUT, this::handleStoredOut);
+
+        extender = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 2);
     }
 
     //Method for IDLE State
     public void handleIdle(StateMetadata<drawerStates> metaData) {
-
+    
+        extender.set(Value.kReverse);
     }
 
     //Method for EMPTYIN State
     public void handleEmptyIn(StateMetadata<drawerStates> metaData) {
-
+    
+        extender.set(Value.kReverse);
     }
 
     //Method for EMPTYOUT State
     public void handleEmptyOut(StateMetadata<drawerStates> metaData) {
 
+        extender.set(Value.kForward);
     }
 
     //Method for STOREDIN State
     public void handleStoredIN(StateMetadata<drawerStates> metaData) {
 
+        extender.set(Value.kReverse);
     }
 
     //Method for STOREDOUT State
     public void handleStoredOut(StateMetadata<drawerStates> metaData) {
 
+        extender.set(Value.kForward);
     }
 
     //Makes the state machine run periodically
