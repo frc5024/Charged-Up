2093002985f069f59b5851951ea5acafd446b508
Vinicius <viniciuscoelho.cv@gmail.com>
2023-01-27 17:36:44 -0500

    Code

---
+++ src/main/java/frc/robot/OI.java
@@ -0,0 +1,22 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+
+
+public class OI {
+    private static OI instance = null;
+   
+    public static OI getInstance(){
+        if (instance == null) {
+            instance = new OI();
+        }
+        return instance;
+    }
+
+
+    private OI() {
+        
+    }
+    
+}
