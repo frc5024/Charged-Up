ceaaf75c7a0ef702f64d817a66071b3047a939de
Vinicius <viniciuscoelho.cv@gmail.com>
2023-02-09 19:19:09 -0500

    Creating the basis of combined intake system

--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -6,25 +6,24 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class DrawerCommand extends CommandBase {
-  /** Creates a new DrawerCommand. */
-  public DrawerCommand() {
-    // Use addRequirements() here to declare subsystem dependencies.
+public class IntakeCommand extends CommandBase {
+ 
+  public IntakeCommand() {
+  
   }
 
-  // Called when the command is initially scheduled.
   @Override
   public void initialize() {}
 
-  // Called every time the scheduler runs while the command is scheduled.
+ 
   @Override
   public void execute() {}
 
-  // Called once the command ends or is interrupted.
+
   @Override
   public void end(boolean interrupted) {}
 
-  // Returns true when the command should end.
+
   @Override
   public boolean isFinished() {
     return false;
