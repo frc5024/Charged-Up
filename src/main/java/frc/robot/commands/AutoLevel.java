9c101d59a2f59bd3901b84e8418032ba5de26654
Connor <quinconn312@gotvdsb.ca>
2023-02-16 17:28:16 -0500

    AutoLevels in Swerve

--- src/main/java/frc/robot/commands/AutoLevel.java
+++ src/main/java/frc/robot/commands/AutoLevel.java
@@ -8,13 +8,16 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants.Swerve;
+import frc.robot.subsystems.Swerve;
+
 
 public class AutoLevel extends CommandBase {
   /** Creates a new AutoLevel. */
   AHRS gyro = new AHRS();
+  private Swerve s_Swerve;
 
   //PID controller
   PIDController pid = new PIDController(0.025, 0, 0.001);
@@ -26,6 +29,8 @@ public class AutoLevel extends CommandBase {
 
   public AutoLevel() {
     // Use addRequirements() here to declare subsystem dependencies.
+    this.s_Swerve = s_Swerve;
+    addRequirements(s_Swerve);
   }
 
   // Called when the command is initially scheduled.
@@ -39,6 +44,7 @@ public class AutoLevel extends CommandBase {
     // pull pitch
     pidreal = pid.calculate(gyro.getPitch(), -1.4);
     // set motor speed
+    s_Swerve.drive(new Translation2d(pidreal, 0), 0, true, false);
     System.out.println(pidreal);
   }
 
