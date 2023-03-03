72c0d7cd2637c1ea765fa5c0030d0d81dce44ae9
Catarina Burghi <56048824+catarinaburghi@users.noreply.github.com>
2023-03-03 16:24:22 -0500

    Update Robot.java
    Adding Cameras for Vision

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -35,6 +35,10 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     ctreConfigs = new CTREConfigs();
+    
+    CameraServer.startAutomaticCapture();
+    CameraServer.startAutomaticCapture();
+    
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
