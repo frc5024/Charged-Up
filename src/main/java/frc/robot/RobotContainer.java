ccd0de06289d57d75c39566f17b4ee476e6fca59
Connor <quinconn312@gotvdsb.ca>
2023-03-03 19:01:00 -0500

    Added strafe and slow mode

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -65,7 +65,7 @@ public class RobotContainer {
         s_Swerve.setDefaultCommand(
                 new TeleopSwerve(s_Swerve, () -> -driver.getRawAxis(translationAxis),
                         () -> -getStrafe(),
-                        () -> -driver.getRawAxis(rotationAxis), () -> robotCentric.getAsBoolean()));
+                        () -> -driver.getRawAxis(rotationAxis), () -> false));
 
         // Configure the button binding
         configureButtonBindings();
